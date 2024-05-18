c28a8994382eacf091a85ddfdee32498d0c34b4c
cnerylozada <cnerylozada@cnerylozadas-MacBook-Pro.local>
2024-05-18 15:53:21 -0500

    feat: web3modal basic setup

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,29 @@
+"use client";
+import { useWeb3Modal } from "@web3modal/wagmi/react";
+import { useAccount, useDisconnect } from "wagmi";
+
+export const Navbar = () => {
+  const { open } = useWeb3Modal();
+  const { disconnect } = useDisconnect();
+  const { address, isDisconnected } = useAccount();
+
+  return (
+    <div className="p-4 flex bg-yellow-100 border-b-2">
+      <div className="w-40">Cristh Dapp</div>
+      <div className="flex-grow flex justify-end">
+        <div>
+          {isDisconnected ? (
+            <button onClick={() => open()}>Open Connect Modal</button>
+          ) : (
+            <div className="flex items-center space-x-4">
+              <div>{address}</div>
+              <div>
+                <button onClick={() => disconnect()}>Disconnect</button>
+              </div>
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
---
+++ config/index.tsx
@@ -0,0 +1,22 @@
+import { defaultWagmiConfig } from "@web3modal/wagmi/react/config";
+import { mainnet, sepolia } from "wagmi/chains";
+
+export const projectId = process.env.NEXT_PUBLIC_PROJECT_ID;
+
+if (!projectId) throw new Error("Project ID is not defined");
+
+const metadata = {
+  name: "cnerylozada",
+  description: "dapp patrick collins",
+  url: "https://web3modal.com", // origin must match your domain & subdomain
+  icons: ["https://avatars.githubusercontent.com/u/37784886"],
+};
+
+// Create wagmiConfig
+const chains = [mainnet, sepolia] as const;
+export const config = defaultWagmiConfig({
+  chains,
+  projectId,
+  metadata,
+  ssr: true,
+});
