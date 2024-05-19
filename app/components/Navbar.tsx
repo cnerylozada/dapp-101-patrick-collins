163d10c15191dbcb759f4b4d702ac0c698f74eee
cnerylozada <cnerylozada@cnerylozadas-MacBook-Pro.local>
2024-05-19 11:27:33 -0500

    feat: navbar was improved

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -10,17 +10,18 @@ export const Navbar = () => {
   const { address } = useAccount();
 
   return (
-    <div className="p-4 flex bg-yellow-100 border-b-2">
-      <div className="w-40">Cristh Dapp</div>
+    <div className="p-4 flex items-center bg-gray-800 border-b-2">
+      <div className="w-40 font-bold text-white">Cristh Dapp</div>
       <div className="flex-grow flex justify-end">
         <div>
           {isWallecConnected ? (
-            <button onClick={() => open()}>Open Connect Modal</button>
+            <button onClick={() => open()} className="text-white">
+              Open Connect Modal
+            </button>
           ) : (
             <div className="flex items-center space-x-4">
-              <div>{address}</div>
               <div>
-                <button onClick={() => disconnect()}>Disconnect</button>
+                <w3m-account-button />
               </div>
             </div>
           )}
