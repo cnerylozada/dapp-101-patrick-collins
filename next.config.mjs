9cd0982abf6dce2489dd6614afa44fd629be8c91
cnerylozada <cnerylozada@cnerylozadas-MacBook-Pro.local>
2024-06-01 08:15:31 -0500

    feat: out folder as build

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
     config.externals.push("pino-pretty", "lokijs", "encoding");
     return config;
   },
-  distDir: "dist",
+  distDir: "out",
 };
 
 export default nextConfig;
